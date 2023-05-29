ab5c026259d97c4d1151e359181a696b7035b545
Jesus Carpintero <jesus.carpintero@adevinta.com>
2023-05-29 16:22:19 +0200

    Fix deprecation of types.AuthConfig

--- backend/docker/docker.go
+++ backend/docker/docker.go
@@ -25,6 +25,7 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/api/types/registry"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
 )
@@ -60,11 +61,11 @@ type Retryer interface {
 }
 
 type registryAuths struct {
-	auths map[string]*types.AuthConfig
+	auths map[string]*registry.AuthConfig
 	mu    sync.RWMutex
 }
 
-func (b *registryAuths) fetchAuth(domain string) (*types.AuthConfig, bool) {
+func (b *registryAuths) fetchAuth(domain string) (*registry.AuthConfig, bool) {
 	domain = getAuthDomain(domain)
 
 	b.mu.RLock()
@@ -73,7 +74,7 @@ func (b *registryAuths) fetchAuth(domain string) (*types.AuthConfig, bool) {
 	return auth, ok
 }
 
-func (b *registryAuths) storeAuth(domain string, auth *types.AuthConfig) {
+func (b *registryAuths) storeAuth(domain string, auth *registry.AuthConfig) {
 	domain = getAuthDomain(domain)
 
 	b.mu.Lock()
@@ -172,7 +173,7 @@ func NewBackend(log log.Logger, cfg config.Config, updater ConfigUpdater) (backe
 		retryer:   re,
 		updater:   updater,
 		auths: registryAuths{
-			auths: make(map[string]*types.AuthConfig),
+			auths: make(map[string]*registry.AuthConfig),
 		},
 	}
 
@@ -190,7 +191,7 @@ func NewBackend(log log.Logger, cfg config.Config, updater ConfigUpdater) (backe
 
 	// Eager validation of the configured registries.
 	for _, a := range b.config.Auths {
-		auth := &types.AuthConfig{
+		auth := &registry.AuthConfig{
 			Username:      a.User,
 			Password:      a.Pass,
 			ServerAddress: a.Server,
@@ -206,7 +207,7 @@ func NewBackend(log log.Logger, cfg config.Config, updater ConfigUpdater) (backe
 }
 
 // addRegistryAuth adds the auth to the map only if valid.
-func (b *Docker) addRegistryAuth(domain string, auth *types.AuthConfig) error {
+func (b *Docker) addRegistryAuth(domain string, auth *registry.AuthConfig) error {
 	if domain == "" {
 		b.log.Debugf("skipping to validate empty auth")
 		return nil
@@ -233,7 +234,7 @@ func (b *Docker) addRegistryAuth(domain string, auth *types.AuthConfig) error {
 // getRegistryAuth tries to find an authentication for the domain
 // First it looks into the provided authenticated servers
 // If not avialiable it looks into the docker system stored credentials.
-func (b *Docker) getRegistryAuth(domain string) *types.AuthConfig {
+func (b *Docker) getRegistryAuth(domain string) *registry.AuthConfig {
 	auth, ok := b.auths.fetchAuth(domain)
 	if ok {
 		return auth
@@ -256,7 +257,7 @@ func (b *Docker) getRegistryAuth(domain string) *types.AuthConfig {
 	return auth
 }
 
-func (b *Docker) getStoredCredentials(domain string) *types.AuthConfig {
+func (b *Docker) getStoredCredentials(domain string) *registry.AuthConfig {
 	domain = getAuthDomain(domain)
 
 	buf := new(bytes.Buffer)
@@ -282,7 +283,7 @@ func (b *Docker) getStoredCredentials(domain string) *types.AuthConfig {
 	}
 
 	// Copy all the data (same struct in different packages).
-	return &types.AuthConfig{
+	return &registry.AuthConfig{
 		Username:      a.Username,
 		Password:      a.Password,
 		Auth:          a.Auth,
